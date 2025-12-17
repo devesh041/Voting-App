e7f97ba41b6990d13a48f240b24c8a4fd78898e7
devesh041 <devesh040107@gmail.com>
2025-12-17 07:13:26 +0530

    error middleware added

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const {connect} = require('mongoose');
 require('dotenv').config();
 
 const Routes = require('./routes/Routes');
+const {notFound, errorHandler} = require('./middleware/errorMiddleware');
 
 const app = express();
 app.use(express.json({extended: true}));
@@ -13,6 +14,8 @@ app.use(cors({extended: true,origin:["http://localhost:3000",]}));
 
 app.use('/api',Routes)
 
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5001;
 
